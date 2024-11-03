03a88c4ba4cfc1bd6f2cf72bd0b53c7a60358a0d
SaltMeister <huisimonliu@gmail.com>
2024-11-03 00:14:36 -0700

    Added templates for home page

--- tpumpRandomizer/randomizer/views.py
+++ tpumpRandomizer/randomizer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("TEST HELLO WORLD")
+    return render(request, "home.html", locals())
